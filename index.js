ebf4b82ef6eaf59bdce2a4e66aef81e2d671a534
Fer Hernandez <fh.1111@gmail.com>
2019-02-05 01:43:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr
+}
+
+function johnLennonFacts(){
+
+
+  
+}
+
+function iLoveTheBeatles(){
+
+
+  
+}
+
